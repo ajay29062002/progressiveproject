250cd4931041e4531a12c513691e2a8e3437dceb
username <wecp-dummy@gmail.com>
2024-07-19 11:45:54 +0000

    Day 4-11

--- backend/src/main/java/com/wecp/progressive/controller/CustomerController.java
+++ backend/src/main/java/com/wecp/progressive/controller/CustomerController.java
@@ -2,60 +2,97 @@ package com.wecp.progressive.controller;
 
 
 import com.wecp.progressive.entity.Customers;
-import com.wecp.progressive.entity.Transactions;
 import com.wecp.progressive.service.CustomerService;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 import java.util.List;
+
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
 
-@Autowired
-CustomerService customerService;
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(@Qualifier("customerServiceImplJpa") CustomerService customerService) {
+        this.customerService = customerService;
+    }
 
     @GetMapping
-    public ResponseEntity<List<Customers>> getAllCustomers() throws SQLException  {
-       return new ResponseEntity<>(customerService.getAllCustomers(),HttpStatus.OK);
-       
-    }
-    @GetMapping("/{customerId}")
-    public ResponseEntity<Customers> getCustomerById(@PathVariable int customerId) throws SQLException {
-        return new ResponseEntity<>(customerService.getCustomerById(customerId),HttpStatus.OK);
-    }
-    @PostMapping
-    public ResponseEntity<Integer> addCustomer(@RequestBody Customers customers) throws SQLException {
-        return new ResponseEntity<>(customerService.addCustomer(customers),HttpStatus.CREATED);
-    }
-    @PutMapping("/{customerId}")
-    public ResponseEntity<Void> updateCustomer(@PathVariable int customerId, @RequestBody Customers customers) throws SQLException {
-        customerService.updateCustomer(customers);
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
-    @DeleteMapping("/{customerId}")
-    public ResponseEntity<Void> deleteCustomer(@PathVariable int customerId) throws SQLException {
-        customerService.deleteCustomer(customerId);
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-   // @GetMapping
-    public ResponseEntity<List<Customers>> getAllCustomerSortedByName() throws SQLException{
-        return new ResponseEntity<>(customerService.getAllCustomersSortedByName(),HttpStatus.OK);
-    }
-    public ResponseEntity<List<Transactions>> getAllTransactionsByCustomerId(int cutomerId) {
-        return null;
-    }
-    @GetMapping("/fromArrayList")
-    public ResponseEntity<List<Customers>> getAllCustomersArraylist(){
-        return new ResponseEntity<>(customerService.getAllCustomersFromArrayList(),HttpStatus.OK);
+    public ResponseEntity<List<Customers>> getAllCustomers() {
+        try {
+            List<Customers> customers = customerService.getAllCustomers();
+            return new ResponseEntity<>(customers, HttpStatus.OK);
+        } catch (SQLException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    // @ExceptionHandler(SQLException.class)
-    // public String handleException(SQLException msg){
-    //     return msg.getMessage();
-    // }
+    @GetMapping("/{customerId}")
+    public ResponseEntity<Customers> getCustomerById(@PathVariable int customerId) {
+        try {
+            Customers customers = customerService.getCustomerById(customerId);
+            if (customers != null) {
+                return new ResponseEntity<>(customers, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (SQLException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Integer> addCustomer(@RequestBody Customers customers) {
+        try {
+            int customerId = customerService.addCustomer(customers);
+            return new ResponseEntity<>(customerId, HttpStatus.CREATED);
+        } catch (SQLException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/{customerId}")
+    public ResponseEntity<Void> updateCustomer(@PathVariable int customerId, @RequestBody Customers customers) {
+        try {
+            customers.setCustomerId(customerId);
+            customerService.updateCustomer(customers);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (SQLException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{customerId}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable int customerId) {
+        try {
+            customerService.deleteCustomer(customerId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (SQLException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/fromArrayList")
+    public ResponseEntity<List<Customers>> getAllCustomersFromArrayList() {
+        List<Customers> customers = customerService.getAllCustomersFromArrayList();
+        return new ResponseEntity<>(customers, HttpStatus.OK);
+    }
+
+    @PostMapping("/toArrayList")
+    public ResponseEntity<List<Customers>> addCustomersToArrayList(@RequestBody Customers customers) {
+        List<Customers> customersList = customerService.addCustomersToArrayList(customers);
+        return new ResponseEntity<>(customersList, HttpStatus.OK);
+    }
+
+    @GetMapping("/fromArrayList/{customerId}")
+    public ResponseEntity<List<Customers>> getAllCustomersSortedByNameFromArrayList() {
+        List<Customers> customersList = customerService.getAllCustomersSortedByNameFromArrayList();
+        return new ResponseEntity<>(customersList, HttpStatus.OK);
+    }
 }
--- backend/src/main/java/com/wecp/progressive/service/CustomerServiceImplJpa.java
+++ backend/src/main/java/com/wecp/progressive/service/CustomerServiceImplJpa.java
@@ -1,115 +1,90 @@
 package com.wecp.progressive.service;
 
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import com.wecp.progressive.entity.Customers;
+import com.wecp.progressive.exception.CustomerAlreadyExistsException;
 import com.wecp.progressive.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Service
-public class CustomerServiceImplJpa implements CustomerService{
+public class CustomerServiceImplJpa implements CustomerService {
 
-//@Autowired
-private final CustomerRepository customerRepository;
+    private final CustomerRepository customerRepository;
 
-@Autowired
-public CustomerServiceImplJpa(CustomerRepository customerRepository) {
-    this.customerRepository = customerRepository;
-}
+    @Autowired
+    public CustomerServiceImplJpa(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
-@Override
-public List<Customers> getAllCustomers() throws SQLException {
-    // TODO Auto-generated method stub
+    private static List<Customers> customersList = new ArrayList<>();
+    @Override
+    public List<Customers> getAllCustomers() {
+        return customerRepository.findAll();
+    }
 
-    return customerRepository.findAll();
-    
- //   throw new UnsupportedOperationException("Unimplemented method 'getAllCustomers'");
-}
+    @Override
+    public Customers getCustomerById(int customerId) {
+        return customerRepository.findByCustomerId(customerId);
+    }
 
-@Override
-public Customers getCustomerById(int customerId) throws SQLException {
-    // TODO Auto-generated method stub
-  
-   return  customerRepository.findById(customerId).get();
-   
-    //throw new UnsupportedOperationException("Unimplemented method 'getCustomerById'");
-}
+    @Override
+    public int addCustomer(Customers customers) {
+        Customers customers1 = customerRepository.findByNameAndEmail(customers.getName(), customers.getEmail());
+        if (customers1 != null) {
+            throw new CustomerAlreadyExistsException("Customer already exists");
+        }
+        return customerRepository.save(customers).getCustomerId();
+    }
 
-@Override
-public int addCustomer(Customers customers) throws SQLException {
-    customerRepository.save(customers);
-   
-        return customers.getCustomerId();
- 
-    // TODO Auto-generated method stub
-   // throw new UnsupportedOperationException("Unimplemented method 'addCustomer'");
-}
+    @Override
+    public void updateCustomer(Customers customers) {
+        customerRepository.save(customers);
+    }
 
-@Override
-public void updateCustomer( Customers customers) throws SQLException {
-    // TODO Auto-generated method stub
+    @Override
+    @Transactional
+    @Modifying
+    public void deleteCustomer(int customerId) {
+        customerRepository.deleteByCustomerId(customerId);
+    }
 
-    customerRepository.save(customers);
-  //  throw new UnsupportedOperationException("Unimplemented method 'updateCustomer'");
-}
-
-@Override
-public void deleteCustomer(int customerId) throws SQLException {
-    // TODO Auto-generated method stub
-    customerRepository.deleteById(customerId);
-   // throw new UnsupportedOperationException("Unimplemented method 'deleteCustomer'");
-}
-
-// @Override
-// public List<Customers> getAllCustomersSortedByName() throws SQLException {
-//     // TODO Auto-generated method stub
-//     return customerRepository.getAllCustomersSortedByName();
- //   throw new UnsupportedOperationException("Unimplemented method 'getAllCustomersSortedByName'");
+    @Override
+    public List<Customers> getAllCustomersSortedByName() {
+        List<Customers> sortedCustomers = customerRepository.findAll();
+        Collections.sort(sortedCustomers);
+        return sortedCustomers;
+    }
 
 
- List<Customers> al = new ArrayList<>();
 
-@Override
-public List<Customers> getAllCustomersFromArrayList() {
-    // TODO Auto-generated method stub
- //   return customerRepository.findAll();
-    return al;
- // throw new UnsupportedOperationException("Unimplemented method 'getAllCustomersFromArrayList'");
-}
+    // The methods mentioned below have to be used for storing and manipulating data in an ArrayList.
+    @Override
+    public List<Customers> getAllCustomersFromArrayList() {
+        return customersList;
+    }
 
-@Override
-public List<Customers> addCustomersToArrayList(Customers customers) {
-    // TODO Auto-generated method stub
-    al.add(customers);
-    return al;
-    
-   // throw new UnsupportedOperationException("Unimplemented method 'addCustomersToArrayList'");
-}
+    @Override
+    public List<Customers> addCustomersToArrayList(Customers customers) {
+        customersList.add(customers);
+        return customersList;
+    }
 
-@Override
-public List<Customers> getAllCustomersSortedByNameFromArrayList() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getAllCustomersSortedByNameFromArrayList'");
-}
+    @Override
+    public List<Customers> getAllCustomersSortedByNameFromArrayList() {
+        List<Customers> sortedCustomers = customersList;
+        Collections.sort(sortedCustomers);
+        return sortedCustomers;
+    }
 
-@Override
-public void emptyArrayList() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'emptyArrayList'");
-}
-
-@Override
-public List<Customers> getAllCustomersSortedByName() throws SQLException {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getAllCustomersSortedByName'");
-}
-
-
-    
-
-
+    @Override
+    public void emptyArrayList() {
+        customersList = new ArrayList<>();
+    }
 }
